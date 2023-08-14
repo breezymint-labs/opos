4ab81db4784978fe25c4b0d9d3b4601265981074
Ansh <anshsaxena4190@gmail.com>
2023-08-14 14:11:23 +0530

    create page content

--- app/(default)/create/page.tsx
+++ app/(default)/create/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import { MuiFileInput } from 'mui-file-input'
+
 export default function create() {
+    const [description, setDescription] = React.useState<string>("");
+    const [name, setName] = React.useState<string>("");
+    const [loading, setLoading] = React.useState(false);
+
+    const [value, setValue] = React.useState(null)
+    const handleChange = (newValue: any) => {
+      setValue(newValue)
+    }
+
+    async function mintCollection() {}
+
     return (
         <section className="relative">
             <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
@@ -7,9 +24,68 @@ export default function create() {
                     <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
                         <h2 className="h2 mb-4">Create Collection</h2>
                         <p className="text-xl text-gray-600">
-                            Fill all the details below and launch your POAP collection now!
+                            Fill all the details below and launch your POAP
+                            collection now!
                         </p>
                     </div>
+
+                    <div className="max-w-xl mx-auto pb-12 md:pb-20">
+                        
+                        <InputLabel id="" sx={{ marginTop: 0 }}>
+                                Name
+                        </InputLabel>
+
+                        <TextField
+                            id="outlined-textarea"
+                            label="Collection Name"
+                            placeholder="Sneak POAP"
+                            multiline
+                            
+                            rows={1}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            sx={{ marginTop: 1 }}
+                        />
+
+                        <InputLabel id="" sx={{ marginTop: 3 }}>
+                                Select Image
+                        </InputLabel>
+
+                        <MuiFileInput 
+                            value={value} 
+                            onChange={handleChange}
+                            placeholder="Collection's Image"
+                            sx={{ marginTop: 1 }}
+                        />
+                        
+                        <InputLabel id="" sx={{ marginTop: 3 }}>
+                                Enter some description
+                        </InputLabel>
+
+                        <TextField
+                            id="outlined-textarea"
+                            label="Metadata description"
+                            placeholder="Description of Image you want to mint"
+                            multiline
+                            fullWidth
+                            rows={2}
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            sx={{ marginTop: 1 }}
+                        />
+
+                        <div className="mt-8 w-full flex justify-center items-center">
+                            <button
+                                onClick={mintCollection}
+                                disabled={loading}
+                                className="py-4 px-10 mx-auto text-white bg-blue-600 hover:bg-blue-700  rounded-md text-sm disabled:opacity-60"
+                            >
+                                Mint Collection 🚀
+                            </button>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
         </section>
